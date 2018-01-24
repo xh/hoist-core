aee61ec0d9b2ce70291eb9beadfda5a39bf93643
febbraiod <donedesign1@gmail.com>
2018-01-24 15:06:05 -0500

    Update action to handles requests from both Sencha and React

--- grails-app/controllers/io/xh/hoist/RestController.groovy
+++ grails-app/controllers/io/xh/hoist/RestController.groovy
@@ -42,8 +42,8 @@ abstract class RestController extends BaseController {
     }
 
     def update() {
-        def data = request.JSON.data
-        preprocessSubmit(data as JSONObject)
+        def data = params.data ? new JSONObject(params.data) : new JSONObject(request.JSON.data)
+        preprocessSubmit(data)
 
         def obj = restTargetVal.get(data.id)
         try {
