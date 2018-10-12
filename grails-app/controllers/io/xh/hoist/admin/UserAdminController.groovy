bc9f53ef4e82e039ad7372a0ea58ba6ec9fa41d4
Anselm McClain <atm@xh.io>
2018-10-11 21:57:08 -0700

    Add role-related endpoints to UserAdminController

--- grails-app/controllers/io/xh/hoist/admin/UserAdminController.groovy
+++ grails-app/controllers/io/xh/hoist/admin/UserAdminController.groovy
@@ -9,15 +9,35 @@ package io.xh.hoist.admin
 
 import io.xh.hoist.BaseController
 import io.xh.hoist.security.Access
+import io.xh.hoist.user.BaseRoleService
 import io.xh.hoist.user.BaseUserService
 
 @Access(['HOIST_ADMIN'])
 class UserAdminController extends BaseController {
 
     BaseUserService userService
+    BaseRoleService roleService
 
-    def index(boolean activeOnly) {
+    def users(boolean activeOnly) {
         renderJSON(userService.list(activeOnly))
+    }
+
+    def roles() {
+        renderJSON(roleService.getAllRoleAssignments())
+    }
+
+    def rolesForUser(String user) {
+        renderJSON(
+            user: user,
+            roles: roleService.getRolesForUser(user)
+        )
+    }
+
+    def usersForRole(String role) {
+        renderJSON(
+            role: role,
+            users: roleService.getUsersForRole(role)
+        )
     }
 
 }
