fb990a6ea76aa22d2396a8810859af34d82a4ce7
Anselm McClain <atm@xh.io>
2018-10-24 15:29:54 -0700

    InstanceConfigUtils support for Docker configs
    + Docker configs / secrets are typically mounted to appear as files within a directory, with each file holding a single config value and named as the config key.

--- src/main/groovy/io/xh/hoist/util/InstanceConfigUtils.groovy
+++ src/main/groovy/io/xh/hoist/util/InstanceConfigUtils.groovy
@@ -11,18 +11,31 @@ import org.yaml.snakeyaml.Yaml
 
 
 /**
- * Utility for loading configuration properties from an external YAML file once on startup and
- * exposing them to the application as a map.
+ * Utility for loading optional, file-based configuration properties once on startup and exposing
+ * them to the application as a map. This utility can be pointed to one of two kinds of files:
+ *
+ *      + A single YAML (.yml) file containing key value pairs (the default).
+ *
+ *      + A directory containing multiple files, where the file names are the keys and the
+ *        file contents are the values. This is intended for use in a Docker environment with
+ *        configs/secrets mounted to a local path within the container.
+ *
+ * To support this second form, all values will be read in and provided as Strings.
  *
  * These are intended to be minimal, low-level configs that apply to a particular deployed instance
  * of the application and therefore are better sourced from a local file/volume vs. source code,
- * JavaOpts, or database-driven ConfigService entries.
+ * JavaOpts, or database-driven ConfigService entries. Expected use-cases include configs for the
+ * primary database connection itself (e.g. host/username/password).
  *
- * The one key guaranteed to be populated is `environment`, which represents the current Hoist
- * AppEnvironment. (This key can also be set via a JavaOpt for backwards compatibility.)
+ * This utility also establishes the `AppEnvironment`, sourcing it from (in priority order):
  *
- * Expected use-cases (apart from environment) could include configs for the primary database
- * connection itself (e.g. host/username/password).
+ *      1) -Dio.xh.hoist.environment JavaOpt, if provided.
+ *      2) Config file/directory entry with key `environment`, if provided.
+ *      3) Fallback to Development, if not otherwise specified.
+ *
+ * The AppEnvironment is made available via its own getter that returns the proper Enum, and is
+ * deliberately removed from the instanceConfig map. (The `io.xh.hoist.util.Utils` method is the
+ * expected entry point for most app code, anyway.)
  *
  * Note that this class is *not* available for use in application.groovy, as that file is read
  * and processed prior to compilation. It can however be read from within a conf/runtime.groovy
@@ -34,21 +47,22 @@ import org.yaml.snakeyaml.Yaml
  */
 class InstanceConfigUtils {
 
-    final static Map instanceConfig = readInstanceConfig()
+    final static Map<String, String> instanceConfig = readInstanceConfig()
+    private static AppEnvironment _appEnvironment
 
-    static getInstanceConfig(String key) {
+    static String getInstanceConfig(String key) {
         return instanceConfig[key]
     }
 
     static AppEnvironment getAppEnvironment() {
-        return instanceConfig.environment
+        return _appEnvironment
     }
 
 
     //------------------------
     // Implementation
     //------------------------
-    private static Map readInstanceConfig() {
+    private static Map<String, String> readInstanceConfig() {
         def ret = [:]
 
         // Attempt to load external config file - but do not require one.
@@ -57,19 +71,32 @@ class InstanceConfigUtils {
                 configFile = new File(configFilename)
 
             if (configFile.exists()) {
-                ret = new Yaml().loadAs(configFile.newInputStream(), Map)
+                ret = configFile.isDirectory() ? loadFromConfigDir(configFile) : loadFromYaml(configFile)
             }
         } catch (ignored) {}
 
-        // Ensure environment key populated - source from (in priority order):
-        // JavaOpt, YAML config, or fallback to 'Development'.
-        def optEnv = System.getProperty('io.xh.hoist.environment')
-        ret.environment = optEnv ?: (ret.environment ?: 'Development')
+        // Populate environment, popping it off the map if provided via config. Priority as documented above.
+        def optEnvString = System.getProperty('io.xh.hoist.environment'),
+            confEnvString = ret.remove('environment'),
+            envString = optEnvString ?: (confEnvString ?: 'Development')
 
-        // Parse environment into enum, falling back to Development again if it can't be parsed.
-        ret.environment = AppEnvironment.parse(ret.environment) ?: AppEnvironment.DEVELOPMENT
+        _appEnvironment = AppEnvironment.parse(envString) ?: AppEnvironment.DEVELOPMENT
 
         return ret
+    }
+
+    private static Map<String, String> loadFromConfigDir(File configDirectory) {
+        def ret = [:]
+        configDirectory.listFiles().each{File it ->
+            if (it.canRead() && !it.isDirectory()) {
+                ret[it.name] = it.text.trim()
+            }
+        }
+        return ret
+    }
+
+    private static Map<String, String> loadFromYaml(File configFile) {
+        return new Yaml().loadAs(configFile.newInputStream(), Map)
     }
 
 }
