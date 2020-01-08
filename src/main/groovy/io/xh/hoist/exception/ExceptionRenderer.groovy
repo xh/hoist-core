6037510349738058c712c1fb82349ee4b899bf57
Lee Wexler <lbwexler@xh.io>
2020-01-08 17:55:40 -0500

    Output all exceptions as JSON + remove obsolete browser lockout  (#100)

--- src/main/groovy/io/xh/hoist/exception/ExceptionRenderer.groovy
+++ src/main/groovy/io/xh/hoist/exception/ExceptionRenderer.groovy
@@ -7,25 +7,22 @@
 
 package io.xh.hoist.exception
 
-import grails.gsp.PageRenderer
 import grails.util.GrailsUtil
 import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import io.xh.hoist.json.JSONFormat
 import io.xh.hoist.json.JSONSerializer
-import io.xh.hoist.util.Utils
 
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 import static org.apache.http.HttpStatus.SC_FORBIDDEN
 import static org.apache.http.HttpStatus.SC_INTERNAL_SERVER_ERROR
+import static org.apache.http.HttpStatus.SC_NOT_FOUND
 
 @CompileStatic
 @Slf4j
 class ExceptionRenderer {
-
-    public template = null
 
     /**
      * Main entry point.  Render an exception to the response.
@@ -33,14 +30,7 @@ class ExceptionRenderer {
     void render(Throwable t, HttpServletRequest request, HttpServletResponse response) {
         GrailsUtil.deepSanitize(t)
         response.setStatus(getHttpStatus(t))
-        switch (getResponseFormat(request)) {
-            case 'HTML':
-                renderAsHTML(t, request, response)
-                break
-            case 'JSON':
-                renderAsJSON(t, request, response)
-                break
-        }
+        renderAsJSON(t, request, response)
         response.flushBuffer()
     }
 
@@ -52,28 +42,10 @@ class ExceptionRenderer {
         response.writer.write(toJSON(t))
     }
 
-    protected void renderAsHTML(Throwable t, HttpServletRequest request, HttpServletResponse response) {
-        response.setContentType('text/html')
-        def isAuth = getHttpStatus(t) == SC_FORBIDDEN,
-            template = isAuth ? '/notAuthorized' : '/exception',
-            model = [
-                title: isAuth ? 'Not Authorized' : 'Exception',
-                exception: t,
-                exceptionAsJSON: toJSON(t)
-        ]
-
-        PageRenderer renderer = (PageRenderer) Utils.appContext.getBean('groovyPageRenderer')
-        String str = renderer.render(model: model, view: template)
-        response.writer.write(str)
-    }
-
     protected int getHttpStatus(Throwable t) {
         if (t instanceof NotAuthorizedException) return SC_FORBIDDEN
+        if (t instanceof NotFoundException) return SC_NOT_FOUND
         return SC_INTERNAL_SERVER_ERROR
-    }
-
-    protected String getResponseFormat(HttpServletRequest r) {
-        return r.getHeader('ACCEPT')?.contains('html') ? 'HTML' : 'JSON'
     }
 
     protected String toJSON(Throwable t) {
@@ -87,5 +59,4 @@ class ExceptionRenderer {
                 ].findAll {it.value}
         return JSONSerializer.serialize(ret);
     }
-    
 }
