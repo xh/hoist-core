fcffd77a5eb50639b96d0c493536eb7826576a24
lbwexler <lbwexler@xh.io>
2020-03-04 15:01:57 -0500

    WIP checkpoint Lee and Peter

--- grails-app/services/io/xh/hoist/log/LogReaderService.groovy
+++ grails-app/services/io/xh/hoist/log/LogReaderService.groovy
@@ -1,12 +1,9 @@
 package io.xh.hoist.log
 
 import io.xh.hoist.BaseService
-import io.xh.hoist.cache.Cache
 import org.apache.commons.io.input.ReversedLinesFileReader
 
 class LogReaderService extends BaseService {
-
-    Cache<String, LengthRecord> fileLengths = new Cache(svc: this)
 
     /**
      * Fetch the (selected) contents of a log file for viewing in the admin console.
@@ -14,95 +11,65 @@ class LogReaderService extends BaseService {
      * @param startLine - (optional) line number of file to start at; if null or zero or negative, will return tail of file
      * @param maxLines - (optional) number of lines to return
      * @param pattern - (optional) only lines matching pattern will be returned
-     * @return - Multi-dimensional array of [linenumber, text] for the requested lines
+     * @return - List of elements of the form [linenumber, text] for the requested lines
      */
-    synchronized readFile(String filename, Integer startLine, Integer maxLines, String pattern) {
-        def tail = !startLine || startLine < 0,
-            ret = [],
-            lineNumber,
-            reader,
+    List readFile(String filename, Integer startLine, Integer maxLines = 10000, String pattern) {
+        def tail = !startLine || startLine <= 0,
+            ret = new ArrayList(maxLines),
             file = new File(LogUtils.logRootPath, filename)
 
+        if (!file.exists()) throw new FileNotFoundException()
 
-        if (!file.exists()) {
-            throw new FileNotFoundException()
-        }
+        Closeable closeable
+        try {
 
-            try {
             if (tail) {
-                lineNumber = getFileLength(file)
-                reader = new ReversedLinesFileReader(file)
+                ReversedLinesFileReader reader = closeable = new ReversedLinesFileReader(file)
+
+                long lineNumber = getFileLength(file)
+                for (String line = reader.readLine(); line != null && ret.size() < maxLines; line = reader.readLine()) {
+                    if (!pattern || line.toLowerCase() =~ pattern.toLowerCase()) {
+                        ret << [lineNumber, line]
+                        lineNumber--
+                    }
+                }
+
             } else {
-                lineNumber = startLine
-                reader = new BufferedReader(new FileReader(file))
-                (1..<lineNumber).each { reader.readLine() }
-            }
-                if (pattern  && )
+                BufferedReader reader = closeable = new BufferedReader(new FileReader(file))
 
+                // Skip lines as needed
+                for (def i = 1; i < startLine; i++) {
+                    def throwAway = reader.readLine();
+                    if (throwAway == null) return []
+                }
 
-                    for (def line = reader.readLine();
-                 line != null && ret.size() < maxLines;
-                 line = reader.readLine()) {
-                if (!pattern || line.toLowerCase() =~ pattern.toLowerCase()) {
-                    ret << [lineNumber, line]
-                    lineNumber += tail ? -1 : 1
+                long lineNumber = startLine
+                for (String line = reader.readLine(); line != null && ret.size() < maxLines; line = reader.readLine()) {
+                    if (!pattern || line.toLowerCase() =~ pattern.toLowerCase()) {
+                        ret << [lineNumber, line]
+                        lineNumber++
+                    }
                 }
             }
 
             return ret
+
         } finally {
-            if(reader) {
-                reader.close()
-            }
+            if (closeable) closeable.close()
         }
     }
 
     long getFileLength(File file) {
-        def path = file.getAbsolutePath()
-        if(!records.containsKey(path)) {
-            records[path] = new LengthRecord(file)
-        }
-        return records[path].getLength()
-    }
-
-    @Override
-    void destroy() {
-        records.values().each {it.destroy()}
-    }
-
-    class LengthRecord {
-        File file
         BufferedReader reader
-        long length = 0
-        long bytes = 0;
-
-        LengthRecord(File file) {
-            this.file = file
+        try {
             reader = new BufferedReader(new FileReader(file))
-        }
-
-        long getLength() {
-            update()
-            return length
-        }
-
-        long update() {
-            if(file.size() < bytes) {
-                log.info('Log file was deleted or truncated, re-reading...')
-                length = 0;
-                bytes = 0
-                reader.close()
-                reader = new BufferedReader(new FileReader(file))
+            long ret = 0;
+            while (reader.readLine() != null) {
+                ret++;
             }
-
-            bytes = file.size()
-
-            while(reader.readLine() != null) length++
-            return length
-        }
-
-        void destroy() {
-            reader.close()
+            return ret
+        } finally {
+            if (reader) reader.close()
         }
     }
 }
