992d6f0de1e1c7378936b7fc88c7eef8c2cd347f
Lee Wexler <lbwexler@xh.io>
2020-07-07 11:19:40 -0400

    Add support for Preference Diffing (#154)
    codev:  Lee and Amanda

---
+++ grails-app/controllers/io/xh/hoist/admin/PreferenceDiffAdminController.groovy
@@ -0,0 +1,31 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2020 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.hoist.admin
+
+import io.xh.hoist.BaseController
+import io.xh.hoist.pref.Preference
+import io.xh.hoist.json.JSONParser
+import io.xh.hoist.security.Access
+
+@Access(['HOIST_ADMIN'])
+class PreferenceDiffAdminController extends BaseController {
+
+    def preferenceDiffService
+
+    def preferences() {
+        def data = Preference.list()
+        renderJSON(data: data)
+    }
+
+    def applyRemoteValues() {
+        def records = params.get('records')
+        preferenceDiffService.applyRemoteValues(JSONParser.parseArray(records))
+
+        renderJSON(success: true)
+    }
+}
