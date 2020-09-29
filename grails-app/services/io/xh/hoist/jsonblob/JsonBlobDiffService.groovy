6a775f29354c567277ee8f9a2a7e93edc19090af
Tom Tirapani <hello@tomtirapani.com>
2020-09-29 11:56:50 +0100

    Add remoting diff functionality to JsonBlob admin

---
+++ grails-app/services/io/xh/hoist/jsonblob/JsonBlobDiffService.groovy
@@ -0,0 +1,47 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2020 Extremely Heavy Industries Inc.
+ */
+package io.xh.hoist.jsonblob
+
+import grails.web.databinding.DataBinder
+import io.xh.hoist.BaseService
+
+class JsonBlobDiffService extends BaseService implements DataBinder {
+
+    void applyRemoteValues(List records) {
+        records.each {rec ->
+            def blob = JsonBlob.findByTypeAndOwnerAndName(rec.type, rec.owner, rec.name),
+                vals = rec.remoteValue
+
+            // create new blob based on remote values
+            if (!blob) {
+                blob = new JsonBlob(vals)
+                blob.lastUpdatedBy = username
+                blob.save(flush:true)
+                log.info("JsonBlob '${blob.name}' created")
+                return
+            }
+
+            // apply remote values to existing config
+            if (vals) {
+                bindData(blob, vals)
+                blob.lastUpdatedBy = username
+                blob.save(flush:true)
+                log.info("JsonBlob '${blob.name}' updated")
+                return
+            }
+
+            // delete local config if applying null remote value
+            if (!vals) {
+                def name = blob.name
+                blob.delete(flush:true)
+                log.info("JsonBlob '${name}' deleted")
+                return
+            }
+        }
+    }
+
+}
