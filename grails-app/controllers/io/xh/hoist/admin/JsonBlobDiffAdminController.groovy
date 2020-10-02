2d85c85475dd9ebaeacc4f196dda490ef0bb4ccf
Tom Tirapani <hello@tomtirapani.com>
2020-10-02 15:00:54 +0100

    Add remote diff functionality to JsonBlob admin (#170)

---
+++ grails-app/controllers/io/xh/hoist/admin/JsonBlobDiffAdminController.groovy
@@ -0,0 +1,32 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2020 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.hoist.admin
+
+import io.xh.hoist.BaseController
+import io.xh.hoist.jsonblob.JsonBlob
+import io.xh.hoist.json.JSONParser
+import io.xh.hoist.security.Access
+
+@Access(['HOIST_ADMIN'])
+class JsonBlobDiffAdminController extends BaseController {
+
+    def jsonBlobDiffService
+
+    def jsonBlobs() {
+        def data = JsonBlob.list()
+        renderJSON(data: data)
+    }
+
+    def applyRemoteValues() {
+        def records = params.get('records')
+        jsonBlobDiffService.applyRemoteValues(JSONParser.parseArray(records))
+
+        renderJSON(success: true)
+    }
+
+}
