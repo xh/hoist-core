c1249d312b755953cca3a77ed8f3686bc1eb2276
lbwexler <lbwexler@xh.io>
2020-10-04 12:13:25 -0400

    Unique Name for json blobs

--- grails-app/domain/io/xh/hoist/jsonblob/JsonBlob.groovy
+++ grails-app/domain/io/xh/hoist/jsonblob/JsonBlob.groovy
@@ -14,9 +14,9 @@ class JsonBlob implements JSONFormat {
     String token = UUID.randomUUID().toString().substring(0, 8)
     String type
     String owner
-    String acl
     String name
     String value
+    String acl
     String description
     Date dateCreated
     Date lastUpdated
@@ -26,8 +26,8 @@ class JsonBlob implements JSONFormat {
         table 'xh_json_blob'
         cache true
         token index: 'idx_xh_json_blob_token'
-        acl type: 'text'
         value type: 'text'
+        acl type: 'text'
         description type: 'text'
     }
 
@@ -35,9 +35,9 @@ class JsonBlob implements JSONFormat {
         token unique: true, blank: false
         type maxSize: 50, blank: false
         owner maxSize: 50, nullable: true, blank: false
-        acl nullable: true
         name unique: ['owner', 'type'], maxSize: 255, blank: false
         value validator: {Utils.isJSON(it) ?: 'default.invalid.json.message'}
+        acl nullable: true
         description nullable: true
         lastUpdatedBy nullable: true
     }
@@ -47,12 +47,12 @@ class JsonBlob implements JSONFormat {
         token: token,
         type: type,
         owner: owner,
-        acl: acl,
         name: name,
         value: value,
+        acl: acl,
         description: description,
         dateCreated: dateCreated,
         lastUpdated: lastUpdated,
-        lastUpdatedBy: lastUpdatedBy,
+        lastUpdatedBy: lastUpdatedBy
     ]}
 }
