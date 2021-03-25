33583f6d86fbc6b8a281951e6c84b9bb0db0a62a
lbwexler <lbwexler@xh.io>
2021-03-24 22:41:08 -0400

    Serialize NaN and Infinity to `null`

---
+++ src/main/groovy/io/xh/hoist/json/serializer/FloatSerializer.java
@@ -0,0 +1,34 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2021 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.hoist.json.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+public class FloatSerializer extends StdSerializer<Float> {
+
+    public FloatSerializer() {
+        this(null);
+    }
+
+    public FloatSerializer(Class<Float> t) {
+        super(t);
+    }
+
+    @Override
+    public void serialize(Float value, JsonGenerator jgen, SerializerProvider provider) throws IOException {
+        if (value.isNaN() || value.isInfinite()) {
+            jgen.writeNull();
+        } else {
+            jgen.writeNumber(value);
+        }
+    }
+}
