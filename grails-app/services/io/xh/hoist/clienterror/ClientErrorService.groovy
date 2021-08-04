deeb132b8bbbc6d9bdcd144b31f8ae15055afbb6
Saba Schuttloffel <saba@xh.io>
2021-08-04 15:31:51 -0700

    Allow client error URL to be optional
    CR: Lee and Anselm

--- grails-app/services/io/xh/hoist/clienterror/ClientErrorService.groovy
+++ grails-app/services/io/xh/hoist/clienterror/ClientErrorService.groovy
@@ -38,7 +38,7 @@ class ClientErrorService extends BaseService implements EventPublisher {
                     device: getDevice(userAgent),
                     appVersion: appVersion ?: Utils.appVersion,
                     appEnvironment: Utils.appEnvironment,
-                    url: url.take(500),
+                    url: url?.take(500),
                     userAlerted: userAlerted
             ]
 
