86e629b63ed8ddf58560188d21a7db8f6581c6aa
Anselm McClain <atm@xh.io>
2021-11-02 18:31:02 -0700

    New admin endpoint to output env vars and JVM system properties
    + See corresponding Hoist React change for new Admin Console tab to display in grid form.

---
+++ grails-app/controllers/io/xh/hoist/admin/EnvAdminController.groovy
@@ -0,0 +1,22 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2021 Extremely Heavy Industries Inc.
+ */
+package io.xh.hoist.admin
+
+import io.xh.hoist.BaseController
+import io.xh.hoist.security.Access
+
+@Access(['HOIST_ADMIN'])
+class EnvAdminController extends BaseController {
+
+    def index() {
+        renderJSON([
+            environment: System.getenv(),
+            properties: System.getProperties()
+        ])
+    }
+
+}
