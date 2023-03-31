2190d61f2100770502169b5b13fbf071a50db1f1
Anselm McClain <atm@xh.io>
2023-03-31 12:30:02 -0700

    Added new `io.xh.hoist.data.filter` package
    + Basic implementations of data-filtering classes and utils - (partially) mirroring their TypeScript equivalents within Hoist React.

---
+++ src/main/groovy/io/xh/hoist/data/filter/Filter.groovy
@@ -0,0 +1,23 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2023 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.hoist.data.filter
+
+/**
+ * Base class for Hoist data package Filters.
+ *
+ * @see FieldFilter
+ * @see CompoundFilter
+ * @see FunctionFilter
+ */
+abstract class Filter {
+    /** Return a function that can be used to test a object. */
+    abstract Closure<Boolean> getTestFn()
+
+    /** True if the provided other Filter is equivalent to this instance.*/
+    abstract boolean equals(Filter other)
+}
---
+++ src/main/groovy/io/xh/hoist/data/filter/FunctionFilter.groovy
@@ -0,0 +1,34 @@
+/*
+ * This file belongs to Hoist, an application development toolkit
+ * developed by Extremely Heavy Industries (www.xh.io | info@xh.io)
+ *
+ * Copyright © 2023 Extremely Heavy Industries Inc.
+ */
+
+package io.xh.hoist.data.filter
+
+/**
+ * Filters via a custom function (closure) specified by the developer.
+ */
+class FunctionFilter extends Filter {
+
+    final private Closure<Boolean> _testFn
+
+    /** Constructor - not typically called by apps - create via {@link Utils#parseFilter} instead. */
+    FunctionFilter(Closure<Boolean> testFn) {
+        if (!testFn) throw new IllegalArgumentException('FunctionFilter requires a `testFn`')
+        this._testFn = testFn
+    }
+
+    Closure<Boolean> getTestFn() {
+        return this._testFn
+    }
+
+    boolean equals(Filter other) {
+        if (other === this) return true
+        return (
+            other instanceof FunctionFilter &&
+                other._testFn == this._testFn
+        )
+    }
+}
