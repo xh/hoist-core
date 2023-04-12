c9f497c650424fc08f4ab3c19d61376e5054e0f6
lbwexler <lbwexler@xh.io>
2023-04-12 18:56:06 -0400

    Checkpoint

--- grails-app/services/io/xh/hoist/monitor/MemoryMonitoringService.groovy
+++ grails-app/services/io/xh/hoist/monitor/MemoryMonitoringService.groovy
@@ -32,10 +32,11 @@ class MemoryMonitoringService extends BaseService {
     private Timer _snapshotTimer
     private Date _lastInfoLogged
 
+    def configService
+
     void init() {
         _snapshotTimer = createTimer(
-            interval: 'xhMemoryMonitorIntervalSecs',
-            intervalUnits: SECONDS,
+            interval: {config.enabled ? config.snapshotInterval * SECONDS: -1},
             runFn: this.&takeSnapshot
         )
     }
@@ -51,10 +52,17 @@ class MemoryMonitoringService extends BaseService {
      * Dump the heap to a file for analysis.
      */
     void dumpHeap(String filename) {
+        def heapDumpDir = config.heapDumpDir
+        if (heapDumpDir == null) {
+            throw new RuntimeException(
+                "Unable to dump heap. Please specify value for 'xhMemoryMonitor.heapDumpDir'"
+            )
+        }
+        filename = heapDumpDir + filename
         if (!filename.endsWith('.hprof')) {
             filename += '.hprof'
         }
-        withInfo('Dumping Heap') {
+        withInfo(['Dumping Heap', [filename: filename]]) {
             HotSpotDiagnosticMXBean mxBean = ManagementFactory.getPlatformMXBean(HotSpotDiagnosticMXBean.class)
             mxBean.dumpHeap(filename, true)
         }
@@ -66,13 +74,13 @@ class MemoryMonitoringService extends BaseService {
     Map takeSnapshot() {
         def newSnap = getStats()
 
-        _snapshots[currentTimeMillis()] = newSnap
+        _snapshots[newSnap.timestamp] = newSnap
 
         // Don't allow snapshot history to grow endlessly - cap @ 1440 samples, i.e. 24 hours of
         // history if left at default config interval of one snap/minute.
         if (_snapshots.size() > 1440) {
-            def oldest = _snapshots.keys().toList().min()
-            _snapshots.remove(oldest)
+            def oldest = _snapshots.min {it.key}
+            _snapshots.remove(oldest.value)
         }
 
         if (newSnap.usedPctMax > 90) {
@@ -92,16 +100,13 @@ class MemoryMonitoringService extends BaseService {
      * Request an immediate garbage collection, with before and after usage snapshots.
      */
     Map requestGc() {
-        def before = takeSnapshot(),
-            start = currentTimeMillis()
-
+        def before = takeSnapshot()
         System.gc()
-
         def after = takeSnapshot()
         return [
             before: before,
             after: after,
-            elapsedMs: currentTimeMillis() - start
+            elapsedMs: after.timestamp - before.timestamp
         ]
     }
 
@@ -109,40 +114,58 @@ class MemoryMonitoringService extends BaseService {
     //------------------------
     // Implementation
     //------------------------
-    private Map getStats() {
+    private Map getStats(Map lastStats) {
         def mb = 1024 * 1024,
+            timestamp = currentTimeMillis(),
+            gcStats = getGCStats(timestamp),
             total = runtime.totalMemory(),
             free = runtime.freeMemory(),
             max = runtime.maxMemory(),
             used = total - free
 
         return [
+            timestamp: timestamp,
             totalHeapMb: roundTo2DP(total / mb),
             maxHeapMb: roundTo2DP(max / mb),
             usedHeapMb: roundTo2DP(used / mb),
             freeHeapMb: roundTo2DP(free / mb),
             usedPctTotal: roundTo2DP((used * 100) / total),
             usedPctMax: roundTo2DP((used * 100) / max),
-            *: getGCStats()
+            *:gcStats
         ]
     }
 
-    private Map getGCStats() {
-        long collectionTime = 0,
-            collectionCount = 0
+    private Map getGCStats(Long timestamp) {
+        long totalCollectionTime = 0,
+            totalCollectionCount = 0
         for (GarbageCollectorMXBean bean : ManagementFactory.getGarbageCollectorMXBeans()) {
-            collectionTime += bean.collectionTime
-            collectionCount += bean.collectionCount
+            totalCollectionTime += bean.collectionTime
+            totalCollectionCount += bean.collectionCount
         }
 
-        def now = currentTimeMillis(),
-            totalTime = now - startupTime.toInstant().toEpochMilli()
+        // Convert to delta's from last snapshot, if there is one.
+        def last = _snapshots.max {it.key}?.value
+
+        long collectionCount = totalCollectionCount - (last ? last.totalCollectionCount : 0),
+            collectionTime = totalCollectionTime - (last ? last.totalCollectionTime : 0),
+            elapsedTime = timestamp - (last ? last.timestamp : startupTime.toInstant().toEpochMilli())
+
+        def avgCollectionTime = collectionCount ? Math.round(collectionTime/collectionCount) : 0
+
+        def pctCollectionTime = elapsedTime ? roundTo2DP((collectionTime*100)/elapsedTime) : null
+
         return [
+            totalCollectionTime: totalCollectionTime,
+            totalCollectionCount: totalCollectionCount,
             collectionTime: collectionTime,
             collectionCount: collectionCount,
-            avgCollectionTime: Math.round(collectionTime/collectionCount),
-            pctCollectionTime: roundTo2DP((collectionTime *100)/totalTime)
+            avgCollectionTime: avgCollectionTime,
+            pctCollectionTime: pctCollectionTime
         ]
+    }
+
+    private Map getConfig() {
+        return configService.getMap('xhMemoryMonitoringConfig')
     }
 
     private double roundTo2DP(v) {
