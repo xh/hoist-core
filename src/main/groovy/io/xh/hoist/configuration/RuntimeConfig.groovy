ceab403906f362dee594834dd092b4f18764e2d7
Colin Rudd <cnrudd@gmail.com>
2023-05-19 18:28:37 -0400

    First cut of using H2 DB based on instance config settings.

--- src/main/groovy/io/xh/hoist/configuration/RuntimeConfig.groovy
+++ src/main/groovy/io/xh/hoist/configuration/RuntimeConfig.groovy
@@ -23,4 +23,25 @@ class RuntimeConfig {
         }
     }
 
+    // Used only in development
+    // when instanceConfig setting `useH2` is true
+    static void h2Config(Script script) {
+        withDelegate(script) {
+            dataSource {
+                pooled = true
+                jmxExport = true
+                driverClassName = "org.h2.Driver"
+                username = "sa"
+                password = ""
+            }
+            environments {
+                development {
+                    dataSource {
+                        dbCreate = "create-drop"
+                        url = "jdbc:h2:mem:devDb;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+                    }
+                }
+            }
+        }
+    }
 }
