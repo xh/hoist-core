8c9f17feecd20f946af918e9e61b725665414bb1
lbwexler <lbwexler@xh.io>
2023-08-18 09:39:55 -0400

    Fix exception handling

--- src/main/groovy/io/xh/hoist/cluster/ClusterTask.groovy
+++ src/main/groovy/io/xh/hoist/cluster/ClusterTask.groovy
@@ -1,6 +1,6 @@
 package io.xh.hoist.cluster
 
-
+import io.xh.hoist.log.LogSupport
 import io.xh.hoist.util.Utils
 import org.apache.hc.core5.http.HttpStatus
 
@@ -8,14 +8,14 @@ import java.util.concurrent.Callable
 
 import static io.xh.hoist.json.JSONSerializer.serialize
 
-abstract class ClusterTask implements Callable<ClusterResponse>, Serializable {
+abstract class ClusterTask implements Callable<ClusterResponse>, Serializable, LogSupport {
 
     ClusterResponse call() {
         try {
             def result = doCall()
             return new ClusterResponse(status: HttpStatus.SC_OK, result: serialize(result))
         } catch (Throwable t) {
-            return Utils.appContext.exceptionRenderer.handleClusterTaskException(t)
+            return Utils.appContext.exceptionRenderer.handleClusterTaskException(t, this)
         }
     }
 
