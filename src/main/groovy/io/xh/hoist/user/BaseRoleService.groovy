956f2af114717ca8917eb6c69a3e7227668326ab
Anselm McClain <atm@xh.io>
2023-12-14 14:45:26 -0800

    Doc comment updates

--- src/main/groovy/io/xh/hoist/user/BaseRoleService.groovy
+++ src/main/groovy/io/xh/hoist/user/BaseRoleService.groovy
@@ -20,18 +20,43 @@ import static io.xh.hoist.util.DateTimeUtils.SECONDS
  * Abstract base service for maintaining a list of HoistUser <-> Role assignments, where roles
  * are returned as simple strings and made available to both server and client code.
  *
- * Applications must define a concrete implementation of this service with the name 'RoleService'.
- * Roles should be defined and sourced based on the app's particular needs.
+ * Applications MUST define a concrete implementation of this service with the name 'RoleService'.
+ * Depending on the application's needs and any pre-existing or external infrastructure around role
+ * management, applications may choose to implement this service in a variety of ways.
  *
- * By way of example, implementations could:
- *      + Query roles from a database, maintained by this app or externally.
- *      + Query LDAP / AD group memberships, and resolve those to roles.
- *      + Leverage configService to provide quick / built-in storage for role mappings.
+ * Option 1 - use Hoist's built-in {@link Role} class and Admin UI to manage roles.
  *
- * Hoist has a requirement for only one special role - "HOIST_ADMIN" - which grants access to the
- * built-in admin functions and enables user impersonation via IdentityService.
+ *      - Hoist provides a `Role` domain class to persist roles and their memberships to the
+ *        app's primary database. Role members can include directly-assigned users, other roles
+ *        (for role inheritance), and optionally external directory groups (pointers to an external
+ *        Active Directory/LDAP instance - see below).
+ *      - Roles and their memberships can be managed via the Hoist Admin Console.
+ *      - The app-level implementation of this service does not need to (re)implement any of its
+ *        protected methods - it can be an "empty" class that extends this base class.
  *
- * Note that the HoistUser `getRoles()` and `hasRole()` methods serve as the primary application
+ * Option 1a - use Hoist Roles with member pointers to an external directory.
+ *
+ *     - Hoist Roles can have members that are pointers to external directory groups.
+ *     - Apps must implement `getUsersForDirectoryGroups()` on their RoleService to resolve these
+ *       identifiers to actual usernames, e.g. by querying an external LDAP/AD instance.
+ *     - Hoist's Admin Console UI can still be used to define Roles and add members in the form of
+ *       directly assigned users (if desired), other roles, and external groups. The provided
+ *       implementation will use the return from `getUsersForDirectoryGroups()` along with Role
+ *       inheritance and direct user assignments to produce a full resolved set of users.
+ *
+ * Option 2 - use a custom implementation of RoleService.
+ *
+ *     - Hoist's built-in Role management (and Admin Console UI) can be disabled via config.
+ *     - Apps must replace the default `generateRoleAssignments()` implementation entirely to read
+ *       role assignments from a source of their choice.
+ *     - If required for efficiency or any other app-specific handling, apps may also override
+ *       `getRolesForUser()` and `getUsersForRole()`.
+ *
+ * Hoist requires only two roles - "HOIST_ADMIN" and "HOIST_ADMIN_READER" - to support access to the
+ * built-in Admin Console and its backing endpoints. Custom application implementations should take
+ * care to define and return these roles for suitable users.
+ *
+ * Note that {@link HoistUser#getRoles} and {@link HoistUser#hasRole} are the primary application
  * entry-point for verifying roles on a given user, reducing or eliminating any need to call an
  * implementation of this service directly.
  */
