0fc9d8ffc0b9f3f0cda8f3d360b21a540628de9e
Lee Wexler <lbwexler@xh.io>
2024-01-18 19:22:32 -0500

    Throw Application Initializing Exception in controllers (#324)
    cr: Greg and ATM

--- src/main/groovy/io/xh/hoist/security/HoistSecurityFilter.groovy
+++ src/main/groovy/io/xh/hoist/security/HoistSecurityFilter.groovy
@@ -8,24 +8,46 @@
 package io.xh.hoist.security
 
 import groovy.transform.CompileStatic
+import io.xh.hoist.exception.ExceptionRenderer
+import io.xh.hoist.exception.RoutineRuntimeException
+import io.xh.hoist.log.LogSupport
 import io.xh.hoist.util.Utils
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @CompileStatic
-class HoistSecurityFilter implements Filter {
+class HoistSecurityFilter implements Filter, LogSupport, ApplicationListener<ApplicationReadyEvent> {
+    private boolean isReady = false
+
     void init(FilterConfig filterConfig) {}
     void destroy() {}
 
     void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
         HttpServletRequest httpRequest = (HttpServletRequest) request
         HttpServletResponse httpResponse = (HttpServletResponse) response
-        BaseAuthenticationService svc = (BaseAuthenticationService) Utils.appContext.getBean('authenticationService')
 
+        if (!isReady) {
+            ExceptionRenderer exceptionRenderer = Utils.exceptionRenderer
+            exceptionRenderer.handleException(
+                new RoutineRuntimeException('Application Initializing. Please wait...'),
+                httpRequest,
+                httpResponse,
+                this
+            )
+            return
+        }
+
+        BaseAuthenticationService svc = Utils.authenticationService
         if (svc.allowRequest(httpRequest, httpResponse)) {
             chain.doFilter(request, response)
         }
     }
 
+    void onApplicationEvent(ApplicationReadyEvent event) {
+        isReady = true
+    }
 }
