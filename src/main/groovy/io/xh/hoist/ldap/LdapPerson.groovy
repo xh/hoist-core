1e3a6f0af427152325bb59c50ed8a682fd90bb16
Colin Rudd <cnrudd@users.noreply.github.com>
2024-06-19 08:14:26 -0400

    LdapService: make searchOne & searchMany methods public. (#362)
    LdapPerson: add more keys, change email to mail.

--- src/main/groovy/io/xh/hoist/ldap/LdapPerson.groovy
+++ src/main/groovy/io/xh/hoist/ldap/LdapPerson.groovy
@@ -2,10 +2,20 @@ package io.xh.hoist.ldap
 
 import javax.management.Attribute
 
-class LdapPerson extends LdapObject{
+/**
+ * Minimal set of attributes returned by a search for objectCategory=Person in Microsoft's AD LDAP implementation.
+ * If you need more attributes, you can extend this class and add them.
+ *
+ * Available attributes can be found here:
+ * https://learn.microsoft.com/en-us/archive/technet-wiki/12037.active-directory-get-aduser-default-and-extended-properties
+ */
+class LdapPerson extends LdapObject {
 
     String name
-    String email
+    String displayname
+    String givenname
+    String sn
+    String mail
 
     static LdapPerson create(Collection<Attribute> atts) {
         def ret = new LdapPerson()
@@ -14,6 +24,6 @@ class LdapPerson extends LdapObject{
     }
 
     static List<String> getKeys() {
-        LdapObject.keys + ['name', 'email']
+        LdapObject.keys + ['name', 'displayname', 'givenname', 'sn', 'mail']
     }
 }
