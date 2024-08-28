059bd32350d84b64d4b52bfc8e24b82f2c4ba789
Greg Solomon <greg@xh.io>
2024-08-28 12:00:46 -0400

    Fix `xhAppStatusCheck` note in Bootstrap

--- grails-app/init/io/xh/hoist/BootStrap.groovy
+++ grails-app/init/io/xh/hoist/BootStrap.groovy
@@ -109,7 +109,7 @@ class BootStrap implements LogSupport {
                 groupName: 'xh.io',
                 note: "Controls client calls to server to poll for new version and/or instance. Supports the following options:\n\n" +
                     "-'interval': Frequency (in seconds) with which the status of the app server should be polled. Value of -1 disables checking.\n" +
-                    "-'mode': Action taken by client upon a new version becoming available. Supports the following options:\n" +
+                    "-'updateMode': Action taken by client upon a new version becoming available. Supports the following options:\n" +
                     "\t+ 'forceReload': Force clients to refresh immediately. To be used when an updated server is known to be incompatible with a previously deployed client.\n" +
                     "\t+ 'promptReload': Show an update prompt banner, allowing users to refresh when convenient.\n" +
                     "\t+ 'silent': No action taken."
