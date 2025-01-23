13008b4b6544bb5fb36847b12459049700fca1f0
Colin Rudd <cnrudd@gmail.com>
2025-01-23 11:28:45 -0500

    support path/value toggle support XPath/JsonPath toggle

--- grails-app/controllers/io/xh/hoist/admin/JsonBlobSearchAdminController.groovy
+++ grails-app/controllers/io/xh/hoist/admin/JsonBlobSearchAdminController.groovy
@@ -8,7 +8,8 @@
 package io.xh.hoist.admin
 
 import com.jayway.jsonpath.JsonPath
-import com.jayway.jsonpath.ReadContext
+import com.jayway.jsonpath.Configuration
+import com.jayway.jsonpath.Option
 import io.xh.hoist.BaseController
 import io.xh.hoist.jsonblob.JsonBlob
 import io.xh.hoist.security.Access
@@ -17,26 +18,33 @@ import io.xh.hoist.security.Access
 class JsonBlobSearchAdminController extends BaseController {
 
     def searchByJsonPath() {
+        Configuration conf = Configuration.builder()
+            .options(Option.ALWAYS_RETURN_LIST).build()
+
         List<JsonBlob> results = JsonBlob.list().findAll { entry ->
-            ReadContext ctx = JsonPath.parse(entry.value)
-            try {
-                def result = ctx.read(params.path)
-                logTrace(result)
-                return result != null
-            } catch (e) {
-                return false
-            }
+            def result = JsonPath.using(conf).parse(entry.value).read(params.path)
+            return result.size() > 0
         }
 
         def ret = results.collect { it ->
-            [type: it.type, token: it.token, name: it.name, owner: it.owner, lastUpdated: it.lastUpdated, json: it.value]
+            [
+                type: it.type,
+                token: it.token,
+                name: it.name,
+                owner: it.owner,
+                lastUpdated: it.lastUpdated,
+                json: it.value
+            ]
         }
         renderJSON(ret)
     }
 
-    def getMatchingNodes(String json, String path) {
-        ReadContext ctx = JsonPath.parse(json)
-        def ret = ctx.read(path)
+    def getMatchingNodes(String json, String path, boolean asPathList) {
+        Configuration conf = asPathList
+            ? Configuration.builder().options(Option.AS_PATH_LIST, Option.ALWAYS_RETURN_LIST).build()
+            : Configuration.defaultConfiguration()
+
+        def ret = JsonPath.using(conf).parse(json).read(path)
         renderJSON(ret)
     }
 
