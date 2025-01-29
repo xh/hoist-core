bad1bb0b408115a3a4e6d854c1cca72a1622e04f
Anselm McClain <atm@xh.io>
2025-01-29 10:45:05 -0800

    Service-level doc comment

--- grails-app/services/io/xh/hoist/admin/JsonSearchService.groovy
+++ grails-app/services/io/xh/hoist/admin/JsonSearchService.groovy
@@ -16,6 +16,17 @@ import io.xh.hoist.jsonblob.JsonBlob
 import io.xh.hoist.pref.Preference
 import io.xh.hoist.pref.UserPreference
 
+/**
+ * Service to provide searching of Hoist artifacts that persist JSON data via JSON Path expressions.
+ *
+ * Includes support for searching the contents of:
+ *   - JSONBlobs
+ *   - AppConfigs of type json
+ *   - UserPreferences of type json
+ *
+ * Used by the search tools added to the Hoist Admin Console in Hoist React v73.
+ * See https://github.com/json-path/JsonPath/blob/master/README.md for syntax help.
+ */
 class JsonSearchService extends BaseService {
     private Configuration matchSearchConf = Configuration.builder()
         .options(
@@ -34,7 +45,7 @@ class JsonSearchService extends BaseService {
             Option.ALWAYS_RETURN_LIST
         ).build()
 
-    List searchBlobs(String path) {
+    List<Map> searchBlobs(String path) {
         List<JsonBlob> matches = JsonBlob.list().findAll { hasPathMatch(it.value, path) }
 
         return matches.collect { it ->
@@ -50,7 +61,7 @@ class JsonSearchService extends BaseService {
         }
     }
 
-    List searchConfigs(String path) {
+    List<Map> searchConfigs(String path) {
         List<AppConfig> jsonConfigs = AppConfig.findAllByValueType('json')
         List<AppConfig> results = jsonConfigs.findAll { hasPathMatch(it.value, path) }
 
@@ -65,9 +76,9 @@ class JsonSearchService extends BaseService {
         }
     }
 
-    List searchUserPreferences(String path) {
+    List<Map> searchUserPreferences(String path) {
         List<Preference> jsonPrefs = Preference.findAllByType('json')
-        List<UserPreference> userPrefs = jsonPrefs.collect { UserPreference.findAllByPreference(it)  }.flatten()
+        List<UserPreference> userPrefs = UserPreference.findAllByPreferenceInList(jsonPrefs)
         List<UserPreference> results = userPrefs.findAll { hasPathMatch(it.userValue, path) }
 
         return results.collect { it ->
